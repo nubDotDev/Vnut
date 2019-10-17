f52f2a6d0af96cc3768ac7ca71ef18b67e834011
nubDotDev <41497304+nubDotDev@users.noreply.github.com>
2019-10-17 19:09:31 -0400

    Wrong Premake fix

--- premake5.lua
+++ premake5.lua
@@ -1,32 +1,115 @@
-project "imgui"
-	kind "StaticLib"
-	language "C"
+workspace "Vnut"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "Vnut/vendor/glad"
+include "Vnut/vendor/GLFW"
+
+project "Vnut"
+	location "Vnut"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "vnutpch.h"
+	pchsource "Vnut/src/vnutpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/Vnut/Vnut.h",
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{prj.name}/vendor/glad/include",
+		"%{prj.name}/vendor/GLFW/include",
+	}
+
+	links
+	{
+		"glad",
+		"GLFW",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VNUT_PLATFORM_WINDOWS",
+			"VNUT_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "VNUT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VNUT_RELEASE"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imgui.cpp",
-		"imgui.h",
-		"imgui_demo.cpp",
-		"imgui_draw.cpp",
-		"imgui_widgets.cpp",
-		"imgui_internal.h",
-		"imconfig.h",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h"
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Vnut/src",
+		"Vnut/vendor",
+		"Vnut/vendor/spdlog/include"
+	}
+
+	links
+	{
+		"Vnut"
 	}
 
 	filter "system:windows"
-		systemversion "latest"
+		cppdialect "C++17"
 		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VNUT_PLATFORM_WINDOWS"
+		}
 
 	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+		defines "VNUT_DEBUG"
+		symbols "On"
 
 	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+		defines "VNUT_RELEASE"
+		optimize "On"
